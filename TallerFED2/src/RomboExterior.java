9b857fbf6c19270d66bb533a4017a2bc847a73ca
user <user@user-PC>
2017-11-22 00:17:03 -0500

    Este proyecto contiene el rombo exterior del taller 3

--- TallerFED2/src/RomboExterior.java
+++ TallerFED2/src/RomboExterior.java
@@ -27,41 +27,76 @@ public class RomboExterior {
 		System.out.println("El rombo exterior es");
 		
 		
-		int mitad=numeros.length-2;
+		int mitad=numeros.length/2;
 		//Esta es la parte superior izquierda de la figura*****************
-		for (int i=0; i<=(numeros.length/2); i++) //Este ya esta hecho*****
+		for (int i=0; i<=numeros.length/2; i++) //Este ya esta hecho*****
 		{
-			for (int j=0;j<mitad-((m/2)-2);j++) 
-			{
-				//nueva_matriz[i][j]=numeros[i][j];
-				//System.out.print(numeros[i][j]+" "); 
-			}
-			//System.out.println("");
-			//mitad--;
-		}
-		
-		//Esta es la parte superior derecha de la figura
-		for (int i=0; i<=numeros.length; i++) 
-		{
-			for (int j=mitad-((m/2)-2);j<numeros.length;j++) //Este ya esta hecho*********
-			{
-				//nueva_matriz[i][j]=numeros[i][j];
-				//System.out.print(numeros[i][j]+" "); 
-			}
-			//System.out.println("");
-			//mitad++;
-		}
-		
-		//Esta es la parte inferior izquierda de la figura
-		for (int i=numeros.length/2; i<numeros.length; i++) 
-		{
-			for (int j=0;j<numeros.length/mitad;j++) 
+			for (int j=0;j<mitad-((m/2)-m/2);j++) 
 			{
 				nueva_matriz[i][j]=numeros[i][j];
-				System.out.print(numeros[i][j]+" "); 
+				//System.out.print(numeros[i][j]+" ");
+			}
+			//System.out.println("");
+			mitad--;
+		}
+		
+		mitad=numeros.length/2;
+		
+		//Esta es la parte superior derecha de la figura
+		for (int i=0; i<numeros.length; i++) //ESTE ESTA HECHO
+		{
+			for (int j=mitad;j<numeros.length;j++) 
+			{
+				nueva_matriz[i][j]=numeros[i][j];
+				//System.out.print(numeros[i][j]+" ");
+			}
+			//System.out.println("");
+			mitad++;
+		}
+		
+		mitad=numeros.length/2;
+		
+		//Esta es la parte inferior izquierda de la figura
+		for (int i=numeros.length/2; i<numeros.length; i++) //Este ya esta hecho
+		{
+			for (int j=0;j<mitad-((m/2)-1);j++) 
+			{
+				nueva_matriz[i][j]=numeros[i][j];
+				//System.out.print(numeros[i][j]+" ");
+				
+			}
+			//System.out.println("");
+			mitad++;
+		}
+		
+		mitad=numeros.length/2;
+		
+		//Esta es la parte inferior derecha de la figura
+		for (int i=numeros.length/2; i<numeros.length; i++) 
+		{
+			for (int j=mitad+((m/2)-1);j<numeros.length;j++) 
+			{
+				nueva_matriz[i][j]=numeros[i][j];
+				//System.out.print(numeros[i][j]+" ");
+			}
+			//System.out.println("");
+			mitad--;
+		}
+		
+		
+		
+		System.out.println("");
+		System.out.println("");
+		for(int i=0;i<numeros.length;i++) {
+			for(int j=0; j<numeros.length;j++) {
+				if(nueva_matriz[i][j]==0)
+				{
+					System.out.print("  ");
+				}else{
+					System.out.print(nueva_matriz[i][j]+" ");
+				     }
 			}
 			System.out.println("");
-			mitad++;
 		}
 		
 
